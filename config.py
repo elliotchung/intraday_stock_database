31bf0a3faac17ba8d6fe8f966697d1de32f38a65
Elliot <101564632+elliotchung@users.noreply.github.com>
2023-05-11 19:07:14 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+API_URL = 'https://api.tradier.com/v1/markets/quotes'
+API_URL_TIME_SALES = 'https://api.tradier.com/v1/markets/timesales'
+access_token = "INSERT YOUR TOKEN"
+
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_USER = 'postgres'
+DB_PASS = 'YOUR PASSWORD'
+DB_NAME = 'testdb'
